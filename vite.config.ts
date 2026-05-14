20dea77563550b546c35ebc79633956512d126d8
ahmeddaoud3880 <ahmeddaoud212103880@gmail.com>
2026-05-14 12:53:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/edu-egypt-connect/",
   server: {
     host: "::",
     port: 8080,
